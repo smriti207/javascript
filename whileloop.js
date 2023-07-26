8b373397ea40ccaeb70e6ee19207ec4a8047a066
smriti <smritiyadav207@gmail>
2023-07-27 05:13:06 +0530

    commited while loop file

---
+++ whileloop.js
@@ -0,0 +1,45 @@
+//1.printing 9,8,7,6,5,4,3,2,1,0
+
+var a=9
+while(a>=0)
+{
+    console.log(a)
+    a--
+}
+
+//2.
+var a=10
+while(a>=1)
+{
+    console.log(a)
+    a--
+}
+
+//3.
+
+var a=1
+while(a<=10)
+{
+    console.log(a)
+    a++
+}
+
+//4.
+
+var a=1
+while(a<=10)
+{
+    console.log(a*2)
+    a++
+}
+
+//5.
+
+var a=0
+console.log("addition")
+while(a<=10)
+{
+    
+    console.log(a + a++)
+    a++
+}
